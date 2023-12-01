704867c698338e35887d4d3b1eaa35bf2ff4f4ef
Ayush-Manware <ayushmanware19@gmail.com>
2023-12-02 01:07:57 +0530

    Completed

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,45 +1,64 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
   const [loginData, setLoginData] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://localhost:2400/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: loginData,
-      });
+      const response = await axios.post(
+        "http://localhost:2400/login",
+        loginData
+      );
+      console.log(response);
 
-      const data = await response.json();
-      console.log(data)
+      const data = await response;
+
+      console.log(data);
+
       if (response.ok) {
         console.log(data.message);
-        localStorage.setItem('token', data.token);
+        localStorage.setItem("token", data.token);
       } else {
-        console.error(data.error);
+        if (data.error) {
+          console.error(data.error && data.error);
+        }
       }
     } catch (error) {
-      console.error('error during login:', error);
+      console.error("error during login:", error);
     }
   };
 
   return (
     <div className="registerDiv">
       <h2>Login</h2>
-       <form>
+      <form>
         <label>Email:</label>
-        <input type="email" value={loginData.email} onChange={(e) => setLoginData({ ...loginData, email: e.target.value })} required />
+        <input
+          type="email"
+          value={loginData.email}
+          onChange={(e) =>
+            setLoginData({ ...loginData, email: e.target.value })
+          }
+          required
+        />
 
         <label>Password:</label>
-        <input type="password" value={loginData.password} onChange={(e) => setLoginData({ ...loginData, password: e.target.value })} required />
+        <input
+          type="password"
+          value={loginData.password}
+          onChange={(e) =>
+            setLoginData({ ...loginData, password: e.target.value })
+          }
+          required
+        />
 
-        <button type="button" onClick={handleLogin} className="btn">Login</button>
+        <button type="button" onClick={handleLogin} className="btn">
+          Login
+        </button>
       </form>
     </div>
   );
